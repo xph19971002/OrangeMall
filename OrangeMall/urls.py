7b83340eafaaa8ba81a8cb183b882ad2d2a6dcd6
madl <15565608583@163.com>
2019-01-24 10:16:36 +0800

    urls添加首页路径

--- OrangeMall/urls.py
+++ OrangeMall/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 import xadmin
 
+from apps.main import views
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
+    url('^$',views.index,name='index'),
     url('account/', include('account.urls')),
     url('detail/', include('detail.urls')),
     url('main/', include('main.urls')),
