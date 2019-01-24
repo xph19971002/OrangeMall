e17d44d32f0dfeba7d7064df7b38242ca97a3402
madl <15565608583@163.com>
2019-01-24 16:38:02 +0800

    1111

--- apps/car/views.py
+++ apps/car/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from apps.main.models import ShopCar
+
+
+def car_list(request):
+    # 获取用户购物车列表
+    cars = ShopCar.objects.filter(shop_id=request.user.id)
+    for car in cars:
+        # 商品图片
+        car.image = car.shop.image_set.filter(shop_id=car.shop.shop_id).first()
+        # 商品总价
+        car.sum_price = car.shop.promote_price * car.number
+    return render(request,'car.html',context={'cars':cars})
+
+
