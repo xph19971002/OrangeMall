83aed177e0c30fad570328fa13f6217082672db6
madl <15565608583@163.com>
2019-01-25 13:46:26 +0800

    init

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from apps.account import views
 
 urlpatterns = [
-    url('login/', views.login, name='login'),
+    url('login/', views.login_view, name='login'),
     url('register', views.register, name='register'),
     url('update/', views.update),
     url('logout/', views.logout, name='logout'),
