50191e65d2763c1a5c7e9ee9c79924dab4080983
luopan000 <44659305+luopan000@users.noreply.github.com>
2019-02-13 20:19:49 +0800

    支付完成跳转，清空购物车功能

--- apps/pay/views.py
+++ apps/pay/views.py
@@ -38,9 +38,9 @@ def our_pay(request):
             # 订单标题
             subject="橘子商城-{}".format(int(trad_no)),
             # 支付成功之后 前端跳转的界面
-            return_url=None,
+            return_url='http://127.0.0.1:8000/car/list1/?order_code={0}'.format(trad_no),
             # 支付成功后台跳转接口
-            notify_url='http://127.0.0.1:8000/car/update/?order_code={0}'.format(trad_no)  # 可选, 不填则使用默认notify url
+            notify_url=None  # 可选, 不填则使用默认notify url
         )
         return redirect(PAY_URL_DEV + order_string)
 
